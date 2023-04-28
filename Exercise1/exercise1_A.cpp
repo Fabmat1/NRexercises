8ecb909ba47c985c5b8beb78a89510df65e64823
Fabmat1 <fabmat1701@gmail.com>
2023-04-28 02:43:50 +0200

    Added three-point stencil function and file saving capability to Ex1

--- Exercise1/exercise1_A.cpp
+++ Exercise1/exercise1_A.cpp
@@ -4,6 +4,7 @@
 #include <immintrin.h>
 #include "chrono"
 #include "iostream"
+#include <fstream>
 
 using namespace std::chrono;
 using namespace std;
@@ -22,14 +23,58 @@ vector<double> generate_logspace(int length, double h, double r, double start=0.
 }
 
 
+vector<double> cubic_function(vector<double> x, double a=1., double b=0., double c=0., double d=0.){
+    const int n = x.size();
+    vector<double> y(n);
+
+    #pragma omp parallel for
+    for(int i = 0; i < n; i++){
+        y[i] = x[i]*x[i]*x[i]*a+x[i]*x[i]*b+c*x[i]+d; // ax^3+bx^2+cx+d
+    }
+    return y;
+}
+
+
+pair<vector<double>, vector<double>> three_point_stencil(const vector<double>& x_arr, const vector<double>& y_arr){
+    const int n = y_arr.size()-2;
+    vector<double> dxdy(n);
+    std::vector<double> truncated_x_arr(n);
+
+    #pragma omp parallel for
+    for(int i = 1; i < n-1; i++){
+        dxdy[i] = (y_arr[i+1]-y_arr[i-1])/(x_arr[i+1]-x_arr[i-1]); // (f(x_(i+1))-f_(x_(i-1)))/(x_(i+1)-x_(i-1))
+        truncated_x_arr[i] = x_arr[i];
+    }
+    return {dxdy, truncated_x_arr};
+}
+
+
+void save_file(vector<double> vector1, vector<double> vector2){
+    ofstream outputFile("../output.txt");
+
+    for (int i = 0; i < vector1.size(); i++)
+    {
+        outputFile << vector1[i] << "\t" << vector2[i] << std::endl;
+    }
+
+    outputFile.close();
+    std::cout << "Saved output." << std::endl;
+}
+
+
+
 int main(){
     auto start = high_resolution_clock::now();
 
     vector<double> logspace = generate_logspace(1000, 1e-10, 1.1, 0);
+    vector<double> y = cubic_function(logspace);
+    auto derivative = three_point_stencil(logspace, y);
+    save_file(derivative.first, derivative.second);
 
     auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start);
+    auto duration = duration_cast<microseconds>(end - start);
     auto elapsed = static_cast<float>(duration.count());
-    std::cout << elapsed/1000 << std::endl;
+    std::cout << "Code took " << elapsed/1000 << "ms to execute." << std::endl;
+    std::cout << "All done! " << std::endl;
     return 1;
 }
